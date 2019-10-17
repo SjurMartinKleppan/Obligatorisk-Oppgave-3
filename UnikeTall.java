9a493c3d46a0a7f9b52f644cdc526621adbe37f8
Sjur Martin Kleppan <49344868+SjurMartinKleppan@users.noreply.github.com>
2019-10-17 12:05:43 +0200

    Add files via upload

---
+++ UnikeTall.java
@@ -0,0 +1,42 @@
+public class UnikeTall {
+    // Deklarasjon av array for lagring av tilfeldige heltall
+
+   /* Konstruktør som mottar arrayets lengde som parameter og
+     oppretter arrayet.  */
+
+   /* Metode som skal undersøke om et gitt tall finnes i arrayet fra før.
+     Tallet det letes etter skal mottas som parameter.
+     Metoden skal returnere true hvis tallet finnes i arrayet.
+     Hvis ikke skal metoden returnere false. */
+
+   /* Metode som skal fylle arrayet med tilfeldige tall mellom 100 og 999,
+     begge grenser inkludert. Metoden skal gjøre dette på en måte
+     som sikrer at alle tallene er forskjellige. Metoden skal ikke ha
+     parametre og heller ikke returnere noen verdi.
+     Tips bruk (int)(Math.random()*(900)+100); for å generere tallene mellom 100 og 999
+   */
+
+        // Metode som finner og returnerer det minste tallet i arrayet.
+
+        // Metode som finner og returnerer det største tallet i arrayet.
+
+   /*  Metode som beregner og returnerer den gjennomstnittlige verdien
+     (double-verdi) av tallene i arrayet.  */
+
+   /* Metode som viser tallene i arrayet i en meldngsboks.
+     I tillegg skal det, i meldingsboks, skrives ut
+     opplysninger om hvilket tall som er minst, hvilket som er størst, og
+     hva som er gjennomsnittsverdien, jfr bildet over. Gjennomsnittverdien
+     skal skrives ut med en desimal. Bruk gjerne String.format("%.2f,tall) til dette.
+     Legg inn mellomrom mellom tallene og skriv ut et passende antall tall
+     per linje. */
+
+
+
+
+   public static void main(String []args){
+
+   }
+}
+
+
